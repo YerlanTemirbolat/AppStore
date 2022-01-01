7202f38bfabc80a1c81b45e9668f417072414d21
Yerlan <erlantemirbolat87@gmail.com>
2022-01-01 17:44:46 +0600

    Add header

--- AppStore/Controller/Today/AppFullScreenController.swift
+++ AppStore/Controller/Today/AppFullScreenController.swift
@@ -24,12 +24,7 @@ class AppFullScreenController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if indexPath.item == 0 {
-            // hack
-            let cell = UITableViewCell()
-            let todayCell = TodayCell()
-            cell.addSubview(todayCell)
-            todayCell.centerInSuperview(size: .init(width: 240, height: 240))
-            return cell
+            return AppFullScreenHeaderCell()
         }
         
         let cell = AppFullscreenDescriptionCell()
@@ -37,7 +32,10 @@ class AppFullScreenController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 450
+        if indexPath.row == 0 {
+            return 450
+        }
+        return super.tableView(tableView, heightForRowAt: indexPath)
     }
     
 //    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
--- AppStore/Controller/Today/TodayController.swift
+++ AppStore/Controller/Today/TodayController.swift
@@ -19,7 +19,13 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
         collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
     }
     
-    var appFullscreenController: UIViewController!
+    var appFullscreenController: AppFullScreenController!
+    
+    var topConstraint: NSLayoutConstraint?
+    var leadingConstraint: NSLayoutConstraint?
+    var widthConstraint: NSLayoutConstraint?
+    var heightConstraint: NSLayoutConstraint?
+
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
@@ -38,15 +44,31 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
         guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
         
         self.startingFrame = startingFrame
-        redView.frame = startingFrame
+        
+        // auto layout constraint animations
+        // 4 anchors
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        topConstraint = redView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
+        leadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
+        widthConstraint = redView.widthAnchor.constraint(equalToConstant: startingFrame.width)
+        heightConstraint = redView.heightAnchor.constraint(equalToConstant: startingFrame.height)
+
+        [topConstraint, leadingConstraint, widthConstraint, heightConstraint].forEach { $0?.isActive = true }
+        self.view.layoutIfNeeded()
+
         redView.layer.cornerRadius = 16
         
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseInOut, animations: {
+
+            self.topConstraint?.constant = 0
+            self.leadingConstraint?.constant = 0
+            self.widthConstraint?.constant = self.view.frame.width
+            self.heightConstraint?.constant = self.view.frame.height
             
-            redView.frame = self.view.frame
+            self.view.layoutIfNeeded()
             
             self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 100)
-            
+
         }, completion: nil)
     }
     
@@ -55,7 +77,19 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseInOut, animations: {
             
-            gesture.view?.frame = self.startingFrame ?? .zero
+            self.appFullscreenController.tableView.contentOffset = .zero
+            
+            //gesture.view?.frame = self.startingFrame ?? .zero
+            guard let startingFrame = self.startingFrame else {
+                return
+            }
+
+            
+            self.topConstraint?.constant = startingFrame.origin.y
+            self.leadingConstraint?.constant = startingFrame.origin.x
+            self.widthConstraint?.constant = startingFrame.width
+            self.heightConstraint?.constant = startingFrame.height
+            self.view.layoutIfNeeded()
             
             self.tabBarController?.tabBar.transform = .identity
             
